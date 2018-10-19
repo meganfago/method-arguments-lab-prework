742328880c83d409f0906220b14953efd8af2ff6
Megan Fago <meganlaynefago@gmail.com>
2018-10-19 21:21:43 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,7 +3,7 @@ puts "Hi, my name is #{name}."
 end
 
 def introduction_with_language (name, language)
-  name = "Megan"
-  language ="Ruby"
+  name = "Dan"
+  language ="Ember.js"
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
